f70cf90acd373be1928720fb92a6a6278842e7e8
Jennifer <jennifereleoje4@gmail.com>
2023-09-19 16:15:48 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-*main - print out output using put
-*
-*Return: always 0
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle'\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
